d1214996a8886c3d29bcf0771e70ead0c350798a
Art Brown <art.brown@qutet.com>
2017-06-02 10:39:53 -0700

    ch04.emmiter_listener s1

--- ch04/emmiter_listener.js
+++ ch04/emmiter_listener.js
@@ -72,7 +72,7 @@ var account1 = new Account1()
 account1.on("balanceChanged", account1.displayBalance)
 account1.on("balanceChanged", account1.checkOverdraw)
 account1.on("balanceChanged", () => {
-  account1.checkGoal(this, 1000)
+  account1.checkGoal(account1, 1000)
 })
 account1.deposit(220)
 account1.deposit(320)
