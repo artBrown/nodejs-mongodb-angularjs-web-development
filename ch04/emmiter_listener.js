b396c5d06841df102bcde3f12a1f1bf7cc0518fa
Art Brown <art.brown@qutet.com>
2017-06-02 10:26:16 -0700

    ch04.emmiter_listener

--- ch04/emmiter_listener.js
+++ ch04/emmiter_listener.js
@@ -12,6 +12,7 @@ function Account() {
   }
 }
 Account.prototype.__proto__ = events.EventEmitter.prototype
+
 function displayBalance() {
   console.log("Account balance: $%d", this.balance)
 }
@@ -35,3 +36,45 @@ account.deposit(220)
 account.deposit(320)
 account.deposit(600)
 account.withdraw(1200)
+
+//// extended
+console.log('')
+class Account1 extends events.EventEmitter {
+  displayBalance() {
+    console.log("Account1 balance: $%d", this.balance)
+  }
+
+  checkOverdraw() {
+    if (this.balance < 0) {
+      console.log("Account1 overdrawn!!!")
+    }
+  }
+
+  checkGoal(acc, goal) {
+    if (acc.balance > goal) {
+      console.log("Goal Achieved!!!")
+    }
+  }
+
+  deposit(amount) {
+    this.balance += amount
+    this.emit('balanceChanged')
+  }
+
+  withdraw(amount) {
+    this.balance -= amount
+    this.emit('balanceChanged')
+  }
+}
+Account1.prototype.balance = 0
+
+var account1 = new Account1()
+account1.on("balanceChanged", account1.displayBalance)
+account1.on("balanceChanged", account1.checkOverdraw)
+account1.on("balanceChanged", (() => {
+  account1.checkGoal(this, 1000)
+}))
+account1.deposit(220)
+account1.deposit(320)
+account1.deposit(600)
+account1.withdraw(1200)
