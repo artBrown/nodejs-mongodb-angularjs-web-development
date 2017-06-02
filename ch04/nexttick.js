5ced77f6696a8078f1c006d75c177101fee07f6f
Art Brown <art.brown@qutet.com>
2017-06-02 09:51:53 -0700

    ch04.nexttick extended

--- ch04/nexttick.js
+++ ch04/nexttick.js
@@ -1,18 +1,59 @@
-var fs = require("fs");
+//// extended
+var fs = require("fs")
 fs.stat("nexttick.js", function (err, stats) {
   if (stats) {
-    console.log("nexttick.js Exists");
+    console.log("fs_1 nexttick.js Exists")
   }
-});
+})
 setImmediate(function () {
-  console.log("Immediate Timer 1 Executed");
-});
+  console.log("Immediate Timer 1 Executed")
+  fs.stat("nexttick.js", function (err, stats) {
+    if (stats) {
+      console.log("im_1.fs nexttick.js Exists")
+    }
+  })
+})
 setImmediate(function () {
-  console.log("Immediate Timer 2 Executed");
-});
+  console.log("Immediate Timer 2 Executed")
+  fs.stat("nexttick.js", function (err, stats) {
+    if (stats) {
+      console.log("im_2.fs nexttick.js Exists")
+    }
+  })
+})
 process.nextTick(function () {
-  console.log("Next Tick 1 Executed");
-});
+  console.log("Next Tick 1 Executed")
+  fs.stat("nexttick.js", function (err, stats) {
+    if (stats) {
+      console.log("nt_1.fs nexttick.js Exists")
+    }
+  })
+})
 process.nextTick(function () {
-  console.log("Next Tick 2 Executed");
-});
+  console.log("Next Tick 2 Executed")
+  fs.stat("nexttick.js", function (err, stats) {
+    if (stats) {
+      console.log("nt_2.fs nexttick.js Exists")
+    }
+  })
+})
+fs.stat("nexttick.js", function (err, stats) {
+  if (stats) {
+    console.log("fs_2 nexttick.js Exists")
+    fs.stat("nexttick.js", function (err, stats) {
+      if (stats) {
+        console.log("fs_2.fs nexttick.js Exists")
+      }
+    })
+  }
+})
+fs.stat("nexttick.js", function (err, stats) {
+  if (stats) {
+    console.log("fs_3 nexttick.js Exists")
+    fs.stat("nexttick.js", function (err, stats) {
+      if (stats) {
+        console.log("fs_3.fs nexttick.js Exists")
+      }
+    })
+  }
+})
