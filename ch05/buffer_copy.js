ba0a2019dffd02a776d26baa0d3e57cc8c2c0349
Art Brown <art.brown@qutet.com>
2017-06-03 07:10:46 -0700

    _/streamingApi.js, s

--- ch05/buffer_copy.js
+++ ch05/buffer_copy.js
@@ -1,24 +1,24 @@
-var alphabet = new Buffer('abcdefghijklmnopqrstuvwxyz');
-console.log(alphabet.toString());
+var alphabet = new Buffer('abcdefghijklmnopqrstuvwxyz')
+console.log(alphabet.toString())
 // copy full buffer
-var blank = new Buffer(26);
-blank.fill();
-console.log("Blank: " + blank.toString());
-alphabet.copy(blank);
-console.log("Blank: " + blank.toString());
+var blank = new Buffer(26)
+blank.fill()
+console.log("Blank: " + blank.toString())
+alphabet.copy(blank)
+console.log("Blank: " + blank.toString())
 // copy part of buffer
-var dashes = new Buffer(26);
-dashes.fill('-');
-console.log("Dashes: " + dashes.toString());
-alphabet.copy(dashes, 10, 10, 15);
-console.log("Dashes: " + dashes.toString());
+var dashes = new Buffer(26)
+dashes.fill('-')
+console.log("Dashes: " + dashes.toString())
+alphabet.copy(dashes, 10, 10, 15)
+console.log("Dashes: " + dashes.toString())
 // copy to and from direct indexes of buffers
-var dots = new Buffer('-------------------------');
-dots.fill('.');
-console.log("dots: " + dots.toString());
+var dots = new Buffer('-------------------------')
+dots.fill('.')
+console.log("dots: " + dots.toString())
 for (var i = 0; i < dots.length; i++) {
   if (i % 2) {
-    dots[i] = alphabet[i];
+    dots[i] = alphabet[i]
   }
 }
-console.log("dots: " + dots.toString());
+console.log("dots: " + dots.toString())
--- ch05/stream_write.js
+++ ch05/stream_write.js
@@ -1,18 +1,18 @@
-var stream = require('stream');
-var util = require('util');
-util.inherits(Writer, stream.Writable);
+var stream = require('stream')
+var util = require('util')
+util.inherits(Writer, stream.Writable)
 function Writer(opt) {
-  stream.Writable.call(this, opt);
-  this.data = new Array();
+  stream.Writable.call(this, opt)
+  this.data = new Array()
 }
-Writer.prototype._write = function(data, encoding, callback) {
-  this.data.push(data.toString('utf8'));
-  console.log("Adding: " + data);
-  callback();
-};
-var w = new Writer();
-for (var i=1; i<=5; i++){  
-  w.write("Item" + i, 'utf8');
+Writer.prototype._write = function (data, encoding, callback) {
+  this.data.push(data.toString('utf8'))
+  console.log("Adding: " + data)
+  callback()
 }
-w.end("ItemLast");
-console.log(w.data);
+var w = new Writer()
+for (var i = 1; i <= 5; i++) {
+  w.write("Item" + i, 'utf8')
+}
+w.end("ItemLast")
+console.log(w.data)
